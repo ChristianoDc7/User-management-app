b24ca9d1c28293fcfb85c46540a0b00b9ce1b281
ChristianoDc7 <christianod97@gmail.com>
2022-03-09 09:52:12 +0300

    counter and two way binding

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count ,setCount] = useState<number>(0)
+
+  const [name , setName] = useState<string>("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{count}</span>
+      <button onClick={()=>{setCount(count+1)}}>Increment </button>
+      <p>
+        two way binding :
+        <input type="text" onChange={(event)=>{setName(event.target.value)}} value={name}/>
+        Your Name is : {name}
+      </p>
     </div>
   );
 }
