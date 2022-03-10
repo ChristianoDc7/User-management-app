2604d4ddb4c165afa8d6d7a2da8ad7b0064c4347
ChristianoDc7 <christianod97@gmail.com>
2022-03-10 09:40:35 +0300

    should

--- src/component/home/card/card.tsx
+++ src/component/home/card/card.tsx
@@ -8,17 +8,18 @@ type Props = {
 const CardL : FunctionComponent <Props> = ({techno}) =>
 
 {
+    const {brand , description} = techno ;
 
   return (
     
         <div className="col s3 m3">
             <div className="card blue-grey darken-1">
                 <div className="card-content white-text">
-                    <span className="card-title">{techno.brand}</span>
-                    <p>{techno.description}</p>
+                    <span className="card-title">{brand}</span>
+                    <p>{description}</p>
                 </div>
                 <div className="card-action">
-                    <a href="#">Learn {techno.brand} </a>
+                    <a href="#">Learn {brand} </a>
                 </div>
             </div>
         </div>
