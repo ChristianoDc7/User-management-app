84ee552ce1fa33de3b74d64758d183bea0f19db2
ChristianoDc7 <christianod97@gmail.com>
2022-03-20 23:44:46 +0300

    authContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
 import './App.css';
-import { Routes , Route,  Outlet, useNavigate} from 'react-router-dom';
+import { Routes , Route, useNavigate} from 'react-router-dom';
 import Home from './component/home/home';
 import About from './component/about/about';
 import Count from './component/count/count';
@@ -9,59 +9,37 @@ import OgtTable from './component/ogt/ogt-table';
 import Navs from './component/navbar/navbar';
 import Login from './component/login/login';
 import { NotFound } from './component/not found/NotFound';
-import Unauthorized from './component/login/unauthorized';
+import { AuthContext } from './services/Auth/Auth-Provider';
+import AuthProvider from './services/Auth/Auth-Provider';
+import ProtectedRoute from './services/Auth/protectedRoute';
+
 
 
 
 function App() {
-  const [user , setUser ] = useState<any>({
-    userName :'',
-    password :''
-  })
-  const {userName , password } = user;
-
-  const navigate = useNavigate()
-  const [isAuth , setAuth] = useState<boolean>(false) 
-  useEffect(()=>{
-    if(userName == 'tino' && password == 'tino'){
-      setAuth(true)
-    }else {
-      setAuth(false)
-    }
-    console.log(user)
-  },[user])
-
-  
-  const Logout = () => {
-    setUser({
-      name : '', 
-      password : ''
-    }) 
-    navigate('/')
-  }
-  
+  const {isAuthenticated, setIsAuthenticated , user , setUser , Logout} = useContext(AuthContext)
   return (
-    <div className="App">
-       <Navs/>
-       {
-         isAuth ? (<button onClick={Logout}>Logout</button>) 
-         : ('')
-       }
-       <div className='routes'>
-          <Routes>
-            <Route path='/' element={<Home isAuth={isAuth}/>} />
-            //Route Protégé
-            <Route element={<ProtectedRoute isAllowed={isAuth} />}>
-                <Route path='count' element={<Count/>} />
-                <Route path='ogt' element={<OgtTable/>} />
-                <Route path='users' element={<Users />} />
-            </Route>
-            <Route path='about' element={<About />} />
-            <Route path='*' element={<NotFound />} />
-            <Route path='login' element={<Login setUser={setUser} user={user}/>}></Route>
-          </Routes>
+        <div className="App">
+            <Navs/>
+            {
+              isAuthenticated ? (<button onClick={Logout}>Logout</button>) 
+              : ('')
+            }
+            <div className='routes'>
+                <Routes>
+                  <Route path='/' element={<Home isAuth={isAuthenticated}/>} />
+                  //Route Protégé
+                  <Route element={<ProtectedRoute isAllowed={isAuthenticated} />}>
+                      <Route path='count' element={<Count/>} />
+                      <Route path='ogt' element={<OgtTable/>} />
+                      <Route path='users' element={<Users />} />
+                  </Route>
+                  <Route path='about' element={<About />} />
+                  <Route path='*' element={<NotFound />} />
+                  <Route path='login' element={<Login />}></Route>
+                </Routes>
+            </div>
         </div>
-    </div>
   );
 }
 
@@ -69,14 +47,3 @@ export default App;
 
 
 
-//protected Routes
-type ProtectedProp = {
-  isAllowed : boolean
-}
-const ProtectedRoute : FunctionComponent<ProtectedProp> = ({isAllowed}) =>
-{
-  if(!isAllowed){
-    return <Unauthorized />
-  }
-  return <Outlet/>
-}
