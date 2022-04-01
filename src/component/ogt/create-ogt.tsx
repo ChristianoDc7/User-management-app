39cb11f04ea754141862c8755565ee49b00c671b
ChristianoDc7 <christianod97@gmail.com>
2022-04-01 12:19:25 +0300

    Create

--- src/component/ogt/create-ogt.tsx
+++ src/component/ogt/create-ogt.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Select } from 'react-materialize';
+import { Checkbox, Row, Select, Table } from 'react-materialize';
 
 const CreateOgt = () => {
     const range = (start: number, end: number) => {
@@ -10,37 +10,96 @@ const CreateOgt = () => {
             "August","September","October","November","December"];
 
     const rub = ["Impôt","Douane"]
+
+    const typeOgt = ["type01","type02","type03"]
+
+    const version = ["1.0","2.1"]
+
+    const item = ["IRSA","IRS"]
   return (
     <>
-       <Select
-            label="Selectionnez l'année"
-       >
-           {
-               range(2005, 2011).map(el=>(
-                   <option key={el} value={el.toString()}>{el}</option>
-               ))
-           }
-       </Select>
-       
-       <Select
-            label="Selectionnez le mois"
-       >
-           {
-               month.map((el,i)=>(
-                   <option key={i} value={(i+1).toString()}>{el}</option>
-               ))
-           }
-       </Select>
+        <Row>
+            <Select
+                    label="Selectionnez l'année"
+            >
+                {
+                    range(2005, 2011).map(el=>(
+                        <option key={el} value={el.toString()}>{el}</option>
+                    ))
+                }
+            </Select>
+            
+            <Select
+                    label="Selectionnez le mois"
+            >
+                {
+                    month.map((el,i)=>(
+                        <option key={i} value={(i+1).toString()}>{el}</option>
+                    ))
+                }
+            </Select>
 
-       <Select
-            label="Selectionnez le rubrique"
-       >
-           {
-               rub.map((el,i)=>(
-                   <option key={i} value={(i).toString()}>{el}</option>
-               ))
-           }
-       </Select>
+            <Select
+                    label="Selectionnez le rubrique"
+            >
+                {
+                    rub.map((el,i)=>(
+                        <option key={i} value={(i).toString()}>{el}</option>
+                    ))
+                }
+            </Select>
+
+            <Select
+                    label="Selectionnez le typeOgt"
+            >
+                {
+                    typeOgt.map((el,i)=>(
+                        <option key={i} value={el}>{el}</option>
+                    ))
+                }
+            </Select>
+
+            <Select
+                    label="Choisissez la version"
+            >
+                {
+                    version.map((el,i)=>(
+                        <option key={i} value={el}>{el}</option>
+                    ))
+                }
+            </Select>
+       </Row>
+       <Row>
+           <Table striped>
+               <thead>
+                   <tr>
+                       <th>Rubrique</th>
+                       <th>Montant</th>
+                       <th>Description</th>
+                       <th>Activation</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   <tr>
+                       <td>
+                       <Select
+                                label="Item"
+                        >
+                            {
+                                item.map((el,i)=>(
+                                    <option key={i} value={el}>{el}</option>
+                                ))
+                            }
+                        </Select>
+                       </td>
+                       <td><input type="text" name="rubrique"  className='validate retrecir'/></td>
+                       <td><input type="text" name="rubrique"  className='validate retrecir'/></td>
+                       <td><Checkbox value={'edit'} label={'edit'}/></td>
+                   </tr>
+               </tbody>
+           </Table>
+       </Row>
+
     </>
   )
 }
